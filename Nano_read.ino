dd10820cb518d33222ad29343cdd7944b53c4edc
MarkTolra <38489484+MarkTolra@users.noreply.github.com>
2018-12-02 09:05:19 +1300

    add Arduino and Teensy code

---
+++ Nano_read.ino
@@ -0,0 +1,91 @@
+// Search for the Tail-race Taniwha
+// Final Year Project
+//
+// Receive board code
+// Written by Mark Tolra
+// October 2018
+
+int numSamples=0;
+long t, t0;
+uint32_t count = 0;
+
+#define LED1  2
+#define LED2  3
+#define LED3  4
+
+byte x;
+
+void setup()
+{
+  Serial.begin(115200);
+
+  ADCSRA = 0;             // clear ADCSRA register
+  ADCSRB = 0;             // clear ADCSRB register
+  ADMUX |= (0 & 0x07);    // set A0 analog input pin
+  ADMUX |= (1 << REFS0);  // set reference voltage
+  ADMUX |= (1 << ADLAR);  // left align ADC value to 8 bits from ADCH register
+
+  // sampling rate is [ADC clock] / [prescaler] / [conversion clock cycles]
+  // for Arduino Uno ADC clock is 16 MHz and a conversion takes 13 clock cycles
+  //ADCSRA |= (1 << ADPS2) | (1 << ADPS0);    // 32 prescaler for 38.5 KHz
+//  ADCSRA |= (1 << ADPS2);                     // 16 prescaler for 76.9 KHz
+//  ADCSRA |= (1 << ADPS1) | (1 << ADPS0);    // 8 prescaler for 153.8 KHz
+  ADCSRA |= (1 << ADPS1);
+
+  ADCSRA |= (1 << ADATE); // enable auto trigger
+  ADCSRA |= (1 << ADIE);  // enable interrupts when measurement complete
+  ADCSRA |= (1 << ADEN);  // enable ADC
+  ADCSRA |= (1 << ADSC);  // start ADC measurements
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED1, OUTPUT);
+  pinMode(LED2, OUTPUT);
+  pinMode(LED3, OUTPUT);
+  
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(LED1, LOW);
+  digitalWrite(LED2, LOW);
+  digitalWrite(LED3, LOW);
+}
+
+ISR(ADC_vect)
+{
+  x = ADCH;  // read 8 bit value from ADC
+//  Serial.println(x);
+  numSamples++;
+}
+  
+void loop()
+{
+//  digitalWrite(LED1, HIGH);
+//  digitalWrite(LED2, HIGH);
+//  digitalWrite(LED3, HIGH);
+  if (x > 18)
+  {
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, HIGH);
+//    delay(50);
+  }
+  else if (x > 6)
+  {
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, LOW);
+//    delay(50);
+  }
+  else
+  {
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+//    delay(50);
+  }
+//  
+  Serial.println(x);
+//  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+//  delay(500);                       // wait for a second
+//  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+//  delay(500);                       // wait for a second
+
+}
---
+++ _3_5_PWM.ino
@@ -0,0 +1,38 @@
+// Search for the Tail-race Taniwha
+// Final Year Project
+//
+// Transmit board code
+// Written by Mark Tolra
+// October 2018
+
+
+#define PWM_IN 10        // the main 150kHz signal
+#define PWM_SD  29      // the "pulse" signal to shut down the chip
+const int ledPin = 13;
+
+void LED_blink()
+{
+  digitalWrite(ledPin, HIGH);   // set the LED on
+  delay(500);                  // wait for a second
+  digitalWrite(ledPin, LOW);    // set the LED off
+  delay(500);                   // wait for a second
+}
+
+void setup() 
+{
+  pinMode(ledPin, OUTPUT);     // set up onboard LED to make sure it's working
+
+  // arguments: pin, frequency in Hertz
+  analogWriteFrequency(PWM_IN, 120000); // 120kHz
+//  analogWriteFrequency(PWM_SD, 5);    // To set the enable pin frequency to set
+                                        // the secondary pulse
+}
+
+void loop() 
+{
+  LED_blink();                  // Visual indicator that the board is on
+  
+  // arguments (pin, value) Value: the duty cycle: between 0 (always off) and 255 (always on)
+  analogWrite(PWM_IN, 127);     // 0 = 0% duty, 127 = 50% duty, 255 = 100% duty
+  analogWrite(PWM_SD, 127);     // 0 = 0% duty, 127 = 50% duty, 255 = 100% duty
+}
